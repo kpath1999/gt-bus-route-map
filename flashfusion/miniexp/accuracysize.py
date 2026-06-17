abfafff5b0e2e47b4506529b29273b83b2fb759d
kpath1999 <kausarpatheryaa@gmail.com>
2026-06-17 13:57:03 -0400

    mini exp placeholders

---
+++ flashfusion/miniexp/accuracysize.py
@@ -0,0 +1,4 @@
+"""
+Seeing how query accuracy changes based on the dataset size.
+Will need to alter the ground truth accordingly.
+"""
---
+++ flashfusion/miniexp/latencychunks.py
@@ -0,0 +1,3 @@
+"""
+As you input more chunks into the input prompt, how does the TTFT and E2E latency change?
+"""
